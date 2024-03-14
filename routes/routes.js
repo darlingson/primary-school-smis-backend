9cd6203b74e8beac2e7e2c3d3d97e3746be8c772
darlingson <dmakuwila@gmail.com>
2024-03-15 01:12:30 +0200

    added crud routes for shool

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,37 @@ router.get('/', (req, res) => {
     });
 });
 
+//crud routes for school
+router.get('/school', (req, res) => {
+    res.send({
+        'message':"schools retrieved"
+    })
+})
+router.get('/school/:id', (req, res) => {
+    res.send({
+        'message':"school with id retrieved"
+    })
+})
+router.post('/school', (req, res) => {
+    res.send({
+        'message':"school added"
+    })
+})
+router.put('/school/:id', (req, res) => {
+    res.send({
+        'message':"school with id updated"
+    })
+})
+router.delete('/school/:id', (req, res) => {
+    res.send({
+        'message':"school with id deleted"
+    })
+})
+
+
+
+
+
+
+
 module.exports = router;
