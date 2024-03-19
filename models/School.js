17311bf34a8d344927339304294ed9c59bb958cb
darlingson <dmakuwila@gmail.com>
2024-03-19 21:34:39 +0200

    create the school and schoolAdmin schemas

---
+++ models/School.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const schoolSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    type:{
+        type: String,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+const SchoolSchema = mongoose.model('SchoolAdmin', schoolSchema);
+module.exports = SchoolSchema;
