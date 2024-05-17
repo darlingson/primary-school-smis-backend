87d9f57501bdebb05ac2d7b4c10a21c060ce0ca6
darlingson <dmakuwila@gmail.com>
2024-05-17 23:23:21 +0200

    completed implementing auth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 var User = require('./models/userModel');
 var bodyParser = require('body-parser');
@@ -23,6 +24,20 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// app.use(bodyParser.json({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  // Set CORS headers
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  // Pass to next layer of middleware
+  next();
+});
 //importing the routes
 const authRoutes = require('./routes/auth');
 const routes = require('./routes/routes');
